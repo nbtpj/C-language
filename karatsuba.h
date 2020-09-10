338deedddb96801e5f6547de3457461b12c08ef3
Nguyen Minh Quang <63718153+nbtpj@users.noreply.github.com>
2020-09-10 23:15:17 +0700

    Update karatsuba.h

--- karatsuba.h
+++ karatsuba.h
@@ -24,22 +24,4 @@ BigInt karatsuba(BigInt num1,BigInt num2)
         return (t2 * pow_10(m * 2) + (t1 - t2 - t0) * pow_10(m) + t0);
 
     }
-}/*
-if (num1 < 10) or (num2 < 10)
-    return num1 × num2
-
-    /* Calculates the size of the numbers. *
-    m = min(size_base10(num1), size_base10(num2))
-    m2 = floor(m / 2)
-    /* m2 = ceil(m / 2) will also work *
-
-    /* Split the digit sequences in the middle. *
-    high1, low1 = split_at(num1, m2)
-    high2, low2 = split_at(num2, m2)
-
-    /* 3 calls made to numbers approximately half the size. *
-    z0 = karatsuba(low1, low2)
-    z1 = karatsuba((low1 + high1), (low2 + high2))
-    z2 = karatsuba(high1, high2)
-
-    return (z2 × 10 ^ (m2 × 2)) + ((z1 - z2 - z0) × 10 ^ m2) + z0*/
+}
